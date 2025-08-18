628518e265a79c8f586f2aed2af310b4f32e60f6
Bojos <bojos86@gmail.com>
2025-08-18 19:30:55 +0000

    Recreate Android project with wrapper

--- android/app/src/main/java/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.bbk_ai_app; // عدّلها إلى باكيج تطبيقك إن كانت مختلفة
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/com/bbk/ai/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bbk_ai_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
