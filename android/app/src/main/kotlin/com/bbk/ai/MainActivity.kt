124d206bcf35b5802f95371ba4664fe65c201853
Bojos <bojos86@gmail.com>
2025-08-19 10:40:51 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/bbk/ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/bbk/ai/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.bbk.ai
+// File: android/app/src/main/kotlin/com/example/bbk_ai_app/MainActivity.kt
+package com.example.bbk_ai_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
